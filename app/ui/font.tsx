c4d938d143505c547319a135ce5338fffb661473
Azis202017 <azisa6980@gmail.com>
2024-01-22 01:39:16 +0700

    added nexxt font lusitana challenge

--- app/ui/font.tsx
+++ app/ui/font.tsx
@@ -1,2 +1,7 @@
-import {Inter} from 'next/font/google';
-export const inter = Inter({subsets:['latin']})
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400'
+});
+export const inter = Inter({ subsets: ['latin'] });
